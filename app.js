da31daa3b8738b3649b198f350f4dccde5544881
salmaan-25 <mohamed.salmaan2004@gmail.com>
2025-07-24 16:56:29 +0530

    Replaced bcrypt with bcryptjs for Render deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { Server } = require('socket.io');
 const http = require('http');
 const fs = require('fs');
